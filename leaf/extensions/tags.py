cfc82704f6260a81a6a54935cccb1701f3a46b63
mudkipdev <mudkip@mudkip.dev>
2023-03-13 09:53:01 -0600

    list command & fix minor bug

--- leaf/extensions/tags.py
+++ leaf/extensions/tags.py
@@ -1,4 +1,5 @@
 from bot import LeafBot
+from utils import Paginator
 from discord.ext import commands
 from discord import app_commands
 import discord
@@ -11,21 +12,47 @@ class TagsCog(commands.GroupCog, name="Tags", group_name="tags"):
     def __init__(self, bot: LeafBot) -> None:
         self.bot = bot
 
+    @app_commands.describe(starting_page = "The page to start on.")
+    @app_commands.command(name = "list", description = "Lists all the tags in the server.")
+    async def list_tags(
+        self,
+        interaction: discord.Interaction,
+        starting_page: Optional[int] = 0,
+        silent: Optional[bool] = False
+    ):
+        tags = await self.bot.database.fetch("SELECT * FROM tags WHERE guild_id = $1", interaction.guild.id)
+        embeds = []
+
+        if not tags:
+            embeds.append(discord.Embed(
+                description = "There are no tags in this server.",
+                color = discord.Color.dark_embed())
+            )
+        else:
+            chunks = discord.utils.as_chunks(tags, 25)
+            for chunk in chunks:
+                embeds.append(discord.Embed(
+                    description = "\n".join([f"• **{tag['name']}** (Uses: {tag['uses']})" for tag in chunk]),
+                    color = discord.Color.dark_embed()
+                ))
+        
+        paginator = Paginator(embeds = embeds, index = starting_page)
+        await paginator.start(interaction, ephemeral = silent)
+
     @app_commands.describe(tag="The name of the tag to view.")
     @app_commands.command(name="view", description="Sends the content of a tag.")
     async def view_tag(
         self,
-        interaction:
-        discord.Interaction,
+        interaction: discord.Interaction,
         tag: str,
         silent: Optional[bool] = False
     ) -> None:
-        record = await self.bot.database.fetchrow(
-            "SELECT content FROM tags WHERE name = $1", tag
+        tag = await self.bot.database.fetchrow(
+            "SELECT content FROM tags WHERE name = $1 AND guild_id = $2", tag, interaction.guild.id
         )
         await interaction.response.send_message(
             embed=discord.Embed(
-                description=record["content"], color=discord.Color.dark_embed()
+                description=tag["content"], color=discord.Color.dark_embed()
             ),
             ephemeral=silent,
         )
@@ -79,7 +106,7 @@ class TagsCog(commands.GroupCog, name="Tags", group_name="tags"):
         silent: Optional[bool] = False
     ) -> None:
         owner_id = await self.bot.database.fetchval(
-            "SELECT owner_id FROM Tags WHERE name = $1;", tag
+            "SELECT owner_id FROM Tags WHERE name = $1 AND guild_id = $2;", tag, interaction.guild.id
         )
 
         if (
---
+++ leaf/utils/pagination.py
@@ -0,0 +1,84 @@
+from discord.ext import commands
+import discord
+import asyncio
+
+from typing import List
+
+__all__ = ("Paginator",)
+
+
+class Paginator:
+    def __init__(self, embeds: List[discord.Embed], index: int = 0) -> None:
+        self.paginated_view = PaginatedView(embeds)
+        self.embeds: List[discord.Embed] = embeds
+        self.index: int = index
+
+    async def start(
+        self,
+        messageable: discord.abc.Messageable | discord.Interaction,
+        *args,
+        **kwargs
+    ) -> None:
+        kwargs["embed"] = self.embeds[self.index]
+        kwargs["view"] = self.paginated_view
+
+        if isinstance(messageable, discord.Interaction):
+            await messageable.response.send_message(*args, **kwargs)
+        elif isinstance(messageable, discord.abc.Messageable):
+            await messageable.send(*args, **kwargs)
+
+        self.paginated_view.set_index(self.index)
+
+
+class PaginatedView(discord.ui.View):
+    def __init__(self, embeds: List[discord.Embed]) -> None:
+        super().__init__(timeout=None)
+        self.embeds: List[discord.Embed] = embeds
+        self.index: int = 0
+
+    @discord.ui.button(custom_id="previous", emoji="◀")
+    async def previous(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        self.set_index(self.index - 1)
+        await self.update(interaction)
+
+    @discord.ui.button(custom_id="page", emoji="🔢")
+    async def page(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        await interaction.response.send_modal(PageModal(self))
+
+    @discord.ui.button(custom_id="next", emoji="▶")
+    async def next(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        self.set_index(self.index + 1)
+        await self.update(interaction)
+
+    async def update(self, interaction: discord.Interaction) -> None:
+        await interaction.response.edit_message(
+            embed=self.embeds[self.index], view=self
+        )
+
+    def set_index(self, index: int) -> None:
+        if not 0 <= index < len(self.embeds):
+            return
+
+        self.previous.disabled = index == 0
+        self.next.disabled = index == len(self.embeds) - 1
+        self.index = index
+
+
+class PageModal(discord.ui.Modal, title="Skip to Page"):
+    page = discord.ui.TextInput(label="Page", required=True)
+
+    def __init__(self, paginated_view: PaginatedView) -> None:
+        super().__init__()
+        self.paginated_view = paginated_view
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        if self.page.value.isdigit():
+            self.paginated_view.set_index(int(self.page.value) - 1)
+
+        await self.paginated_view.update(interaction)
