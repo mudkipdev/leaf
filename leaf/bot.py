bdb07506983c169cf75e07465c1ac9864a7a7483
bsrecker <brandonsrecker@gmail.com>
2023-03-30 18:22:11 -0500

    Owner logging.
    Nothing fancy was included here, we are just logging errors to a user provided webhook
    
    Signed-off-by: bsrecker <brandonsrecker@gmail.com>

--- leaf/bot.py
+++ leaf/bot.py
@@ -1,8 +1,14 @@
 __all__ = ("LeafBot",)
 
-from discord.ext import commands, tasks
+from discord import Interaction
+from discord._types import ClientT
+from discord.app_commands import AppCommandError
+from discord.ext import tasks
 import discord
 import asyncpg
+import logging
+from discord.ext import commands
+from discord_logging.handler import DiscordHandler
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,6 +18,8 @@ class LeafBot(commands.Bot):
     def __init__(self, config: dict) -> None:
         self.config = config
         self.database = None
+        self.webhook_url = config["logging"]["webhook_url"]
+        self.logger = logging.getLogger()
 
         super().__init__(
             intents=intents,
@@ -20,11 +28,50 @@ class LeafBot(commands.Bot):
             allowed_mentions=discord.AllowedMentions(everyone=False),
         )
 
+        self.discord_handler = self.setup_logging(self.webhook_url)
+
+    async def send_guild_stats(self, e, guild):
+        e.add_field(name="Name", value=guild.name)
+        e.add_field(name="ID", value=guild.id)
+        e.add_field(name="Shard ID", value=guild.shard_id or "N/A")
+        e.add_field(name="Owner", value=f"{guild.owner} (ID: {guild.owner_id})")
+
+        bots = sum(m.bot for m in guild.members)
+        total = guild.member_count
+        online = sum(m.status is discord.Status.online for m in guild.members)
+        e.add_field(name="Members", value=str(total))
+        e.add_field(name="Bots", value=f"{bots} ({bots/total:.2%})")
+        e.add_field(name="Online", value=f"{online} ({online/total:.2%})")
+
+        if guild.icon:
+            e.set_thumbnail(url=guild.icon_url)
+
+        if guild.me:
+            e.timestamp = guild.me.joined_at
+
+        hook = discord.Webhook.from_url(self.webhook_url, client=self)
+
+        await hook.send(embed=e)
+
     async def setup_hook(self) -> None:
         for extension in self.config["extensions"]:
             await self.load_extension(extension)
 
         self.database = await asyncpg.connect(self.config["database"]["connection_uri"])
+
+    def setup_logging(self, webhook_url: str):
+        log_format = "[%(asctime)s] %(levelname)s %(name)s: %(message)s"
+        discord_format = logging.Formatter(log_format)
+
+        discord_handler = DiscordHandler(
+            service_name=self.config["logging"]["bot_name"], webhook_url=webhook_url
+        )
+        discord_handler.setFormatter(discord_format)
+
+        self.logger.setLevel(self.config["logging"]["logging_level"])
+        self.logger.addHandler(discord_handler)
+
+        return discord_handler
 
     @tasks.loop(minutes=1.0)
     async def update_activity(self) -> None:
@@ -35,6 +82,11 @@ class LeafBot(commands.Bot):
                 + ("s" if len(self.guilds) != 1 else ""),
             )
         )
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        e = discord.Embed(colour=0x53DDA4, title="New Guild")
+        await self.send_guild_stats(e, guild)
 
     async def on_ready(self) -> None:
         self.update_activity.start()
